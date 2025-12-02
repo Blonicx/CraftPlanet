18125e46580e03b94dda91351051cda7b441a42f
Blonicx <belte_yt@outlook.com>
2025-12-02 18:56:12 +0100

    Small Adjustments

--- src/main/java/com/blonicx/craftplanet/integration/config/Config.java
+++ src/main/java/com/blonicx/craftplanet/integration/config/Config.java
@@ -5,9 +5,15 @@ public class Config {
     public boolean filterChat = true;
 
     // Performance
+
+    // General
     public boolean disableWeatherRendering = true;
+
+    // Particle
+    public int maxParticles = 5000;
+
+    // Render Distance
     public int maxSignRenderDistance = 16;
     public int maxEntityRenderDistance = 64;
     public int maxBlockEntityRenderDistance = 64;
-    public int maxParticles = 5000;
 }
