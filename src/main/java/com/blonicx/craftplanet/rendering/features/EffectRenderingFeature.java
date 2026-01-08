acac0c9f7e71da95e78bb0649ec8205fc4929384
Blonicx <belte_yt@outlook.com>
2026-01-08 16:06:42 +0100

    Updated EffectRenderingFeature

--- src/main/java/com/blonicx/craftplanet/rendering/features/EffectRenderingFeature.java
+++ src/main/java/com/blonicx/craftplanet/rendering/features/EffectRenderingFeature.java
@@ -1,20 +1,19 @@
-package com.blonicx.craftplanet.rendering;
+package com.blonicx.craftplanet.rendering.features;
 
 import net.minecraft.client.render.entity.feature.EnergySwirlOverlayFeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.LoadedEntityModels;
 import net.minecraft.client.render.entity.state.BipedEntityRenderState;
 import net.minecraft.util.Identifier;
 
-public class EffectRenderingFeature <S extends BipedEntityRenderState, M extends BipedEntityModel<S>, A extends BipedEntityModel<S>> extends EnergySwirlOverlayFeatureRenderer<S, M> {
-    private final BipedEntityModel model;
-    private static final Identifier SKIN = Identifier.ofVanilla("textures/entity/creeper/creeper_armor.png");
+public class EffectRenderingFeature<S extends BipedEntityRenderState, M extends BipedEntityModel<S>> extends EnergySwirlOverlayFeatureRenderer<S, M> {
 
-    public EffectRenderingFeature(FeatureRendererContext<S, M> context, LoadedEntityModels loader, BipedEntityModel model) {
+    private static final Identifier SKIN = Identifier.ofVanilla("textures/entity/creeper/creeper_armor.png");
+    private final M model;
+
+    public EffectRenderingFeature(FeatureRendererContext<S, M> context, M model) {
         super(context);
-        this.model = new BipedEntityModel(loader.getModelPart(EntityModelLayers.CREEPER_ARMOR));
+        this.model = model;
     }
 
     @Override
@@ -22,15 +21,18 @@ public class EffectRenderingFeature <S extends BipedEntityRenderState, M extends
         return true;
     }
 
+    @Override
     protected float getEnergySwirlX(float partialAge) {
         return partialAge * 0.01F;
     }
 
+    @Override
     protected Identifier getEnergySwirlTexture() {
         return SKIN;
     }
 
+    @Override
     protected M getEnergySwirlModel() {
-        return (M) this.model;
+        return this.model;
     }
 }
