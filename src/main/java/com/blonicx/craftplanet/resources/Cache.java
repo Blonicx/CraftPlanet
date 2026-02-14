b0687e1354d83fc5d22fc20a99242bbec288107a
Blonicx <belte_yt@outlook.com>
2026-02-14 18:01:52 +0100

    Added Clear Cache

--- src/main/java/com/blonicx/craftplanet/resources/Cache.java
+++ src/main/java/com/blonicx/craftplanet/resources/Cache.java
@@ -1,9 +1,21 @@
 package com.blonicx.craftplanet.resources;
 
+import com.blonicx.craftplanet.CraftPlanet;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.File;
+import java.util.Objects;
 
 public class Cache {
     public static final String CACHE_DIR = new File(FabricLoader.getInstance().getConfigDir().toFile(), "craftplanet/cache/").toString();
+
+    public static void clearCache() {
+        File[] cache = new File(CACHE_DIR).listFiles();
+
+        for (File file : Objects.requireNonNull(cache)) {
+            file.delete();
+        }
+
+        CraftPlanet.LOGGER.info("Cleared {} files from the {} Cache!", cache.length, CraftPlanet.MOD_ID);
+    }
 }
