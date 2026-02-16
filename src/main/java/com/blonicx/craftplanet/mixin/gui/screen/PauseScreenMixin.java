0660cd0ebd8d3be61d8ee74dee9e06487396d9c6
Blonicx <belte_yt@outlook.com>
2026-02-16 20:37:10 +0100

    Added a Current Time Text

---
+++ src/main/java/com/blonicx/craftplanet/mixin/gui/screen/PauseScreenMixin.java
@@ -0,0 +1,30 @@
+package com.blonicx.craftplanet.mixin.gui.screen;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.StringWidget;
+import net.minecraft.client.gui.screens.PauseScreen;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Mixin(PauseScreen.class)
+public class PauseScreenMixin extends Screen {
+    protected PauseScreenMixin(Component component) {
+        super(component);
+    }
+
+    @Inject(method = "init", at = @At("HEAD"))
+    void init(CallbackInfo ci) {
+        LocalTime now = LocalTime.now();
+        String time = now.format(DateTimeFormatter.ofPattern("HH:mm"));
+        int w = this.font.width(this.title);
+
+        this.addRenderableWidget(new StringWidget(this.width / 2 - w / 4, 50, w, 9, Component.literal(time), this.font));
+    }
+}
