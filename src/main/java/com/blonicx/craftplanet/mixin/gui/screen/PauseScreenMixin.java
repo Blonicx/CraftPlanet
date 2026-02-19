016f8d8b5228391db2c926b97e84843bb8001951
Blonicx <Belte_YT@outlook.com>
2026-02-20 00:17:10 +0100

    Updated Time Info

--- src/main/java/com/blonicx/craftplanet/mixin/gui/screen/PauseScreenMixin.java
+++ src/main/java/com/blonicx/craftplanet/mixin/gui/screen/PauseScreenMixin.java
@@ -1,10 +1,12 @@
 package com.blonicx.craftplanet.mixin.gui.screen;
 
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.StringWidget;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,16 +20,26 @@ public class PauseScreenMixin extends Screen {
         super(component);
     }
 
+    @Unique
+    StringWidget timeWidget;
+
     @Inject(method = "init", at = @At("HEAD"))
     void init(CallbackInfo ci) {
-        LocalTime now = LocalTime.now();
-        String time = now.format(DateTimeFormatter.ofPattern("HH:mm"));
         int w = this.font.width(this.title);
 
         //? if >= 1.21.9 {
-        this.addRenderableWidget(new StringWidget(this.width / 2 - w / 4, 50, w, 9, Component.literal(time), this.font));
+        timeWidget = new StringWidget(this.width / 2 - w / 4, 50, w, 9, Component.literal(""), this.font);
          //?} else {
-        /*this.addRenderableWidget(new StringWidget(this.width / 2 - w / 2, 50, w, 9, Component.literal(time), this.font));
+        /*timeWidget = new StringWidget(this.width / 2 - w / 2, 50, w, 9, Component.literal(""), this.font);
         *///?}
+        this.addRenderableWidget(timeWidget);
     }
+
+    @Inject(method = "render", at = @At("HEAD"))
+    void render(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci) {
+        LocalTime now = LocalTime.now();
+        String time = now.format(DateTimeFormatter.ofPattern("HH:mm"));
+        timeWidget.setMessage(Component.literal(time));
+    }
+
 }
